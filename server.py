950d0f405c26c4bd13b666b949a6efa686fccd04
swynnejr <saucersam@gmail.com>
2021-07-09 16:54:54 -0500

    basics complete

--- server.py
+++ server.py
@@ -3,8 +3,6 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def hello_world():
     return 'Hello World!'  # Return the string 'Hello World!' as a response
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module
-    app.run(debug=True)    # Run the app in debug mode.
 
 # import statements, maybe some other routes
 
@@ -12,5 +10,33 @@ if __name__=="__main__":   # Ensure this file is being run directly and not from
 def success():
     return "success"
 
+@app.route('/hello/<name>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+def hello(name):
+    print(name)
+    return "Hello, " + name
+
+@app.route('/users/<username>/<id>') # for a route '/users/____/____', two parameters in the url get passed as username and id
+def show_user_profile(username, id):
+    print(username)
+    print(id)
+    return "username: " + username + ", id: " + id
+
+@app.route('/dojo')
+def dojo():
+    return 'dojo'
+
+@app.route('/say/<name>')
+def hi(name):
+    print(name)
+    return "Hi " + name +"!"
+
+@app.route('/repeat/<multiple>/<statement>')
+def repeat_yo_self(multiple, statement):
+    print(multiple)
+    print(statement)
+    return statement * int(multiple)
+
 # app.run(debug=True) should be the very last statement!
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module
+    app.run(debug=True)    # Run the app in debug mode.
 
