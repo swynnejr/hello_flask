bcd1de35c6903dcfcdf1b3873393b4e24b5f5c7c
swynnejr <saucersam@gmail.com>
2021-07-09 17:36:16 -0500

    accidentally added the next lesson

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
-from flask import Flask  # Import Flask to allow us to create our app
+from flask import Flask, render_template  # Import Flask to allow us to create our app
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
+    # return 'Hello World!'  # Return the string 'Hello World!' as a response
+    return render_template('index.html')
+
+
 
 # import statements, maybe some other routes
 
